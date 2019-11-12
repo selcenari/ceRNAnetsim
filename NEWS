4cb6a545095799c0f704d061e8ebf0bbb3cec526
Selcen Ari <selcenarii@gmail.com>
2019-11-12 15:40:42 +0300

    deleting services branch

--- NEWS
+++
@@ -1,3 +0,0 @@
-Changes in ceRNAnetsim (0.99.0) package (2019-10-7)
-
-+ Submission to Bioconductor
--- R/globals.R
+++
@@ -1,5 +0,0 @@
-utils::globalVariables(c("name", "count_current", "initial_count", ".", "from", "comp_count_list",
-                         "dif", "non_zero", "effect", "iter", "nodes", "eff_count", "effect_current", "comp_count_current",
-                         "mirna_count_current", "Competing_name", "comp_count_pre", "miRNA_name", "dummy", "competing", "miRNA",
-                         "nodes", "to", "afff_factor", "mirna_count_per_dep", "degg_factor", "mirna_count_per_comp", "effect_list",
-                         "mirna_count_list", "count_pre", "type", "node_id", "changes_variable", "x", "y", "mirna_count_pre", "effect_pre", "len"))
